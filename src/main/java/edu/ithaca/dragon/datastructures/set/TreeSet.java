5614ca56a07a6b03784b71d73a8339243a2c7632
ghoman <griffhoho@gmail.com>
2022-10-27 11:24:09 -0400

    Created ListSet and TreeSet classes

---
+++ src/main/java/edu/ithaca/dragon/datastructures/set/TreeSet.java
@@ -0,0 +1,35 @@
+package edu.ithaca.dragon.datastructures.set;
+
+import java.util.ArrayList;
+
+public class TreeSet<T extends Comparable<T>> implements Set<T>{
+    @Override
+    public void add(T item) {
+
+    }
+
+    @Override
+    public boolean contains(T item) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public ArrayList<T> toOrderedList() {
+        return null;
+    }
+
+    @Override
+    public Set<T> union(Set<T> otherSet) {
+        return null;
+    }
+
+    @Override
+    public Set<T> intersection(Set<T> otherSet) {
+        return null;
+    }
+}
